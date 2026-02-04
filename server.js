c9f42439e750eca42aa23d859fdf673509989d32
2100031497cseh-web <2100031497cseh@gmail.com>
2026-02-05 01:53:37 +0530

    Create server.js main entry point for Express backend

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-// server.js
-
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
